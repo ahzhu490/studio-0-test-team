6c0724233a26a7c8299f6d4dbf3196aa8725918e
Your Name <a.h.zhu@wustl.edu>
2025-08-28 00:53:47 -0500

    good

--- src/studio0/LunchReceipt.java
+++ src/studio0/LunchReceipt.java
@@ -8,10 +8,11 @@ public class LunchReceipt {
 		double drinkPrice = 1.25;
 		double chipsPrice = 1.00;
 		double guacPrice = 2.50;
-		
 		double subtotal = burritoPrice + drinkPrice + chipsPrice + guacPrice;
 		double taxRate = 0.04225;
+
 		double taxAmount = subtotal * taxRate;
+
 		double total = subtotal + taxAmount;
 		
 		System.out.println("Your total was: $" + total);
